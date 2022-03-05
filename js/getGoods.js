65603be47c3b8119d9212d4505a855c670ec0cfb
Sergey Trukhan <truhan_1984@mail.ru>
2022-03-05 19:50:50 +0300

    commit

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,24 +1,48 @@
 const getGoods = () => {
   const links = document.querySelectorAll(".navigation-link");
+  const more = document.querySelector(".more");
 
   const renderGoods = (goods) => {
-    console.log(goods);
+    const goodsContainer = document.querySelector(".long-goods-list");
+
+    goodsContainer.innerHTML = "";
+
+    goods.forEach((good) => {
+      const goodBlock = document.createElement("div");
+
+      goodBlock.classList.add("col-lg-3");
+      goodBlock.classList.add("col-sm-6");
+
+      goodBlock.innerHTML = `
+			<div class="goods-card">
+				<span class="label ${good.label ? null : "d-none"}">${good.label}</span>
+				<img src="db/${good.img}" alt="${good.name}" class="goods-image" />
+				<h3 class="goods-title">${good.name}</h3>		
+				<p class="goods-description">${good.description}</p>		
+				<button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+					<span class="button-price">$${good.price}</span>
+				</button>	
+			</div>
+		`;
+
+      goodsContainer.append(goodBlock);
+    });
   };
 
   const getData = (value, category) => {
     fetch("/db/db.json")
       .then((res) => res.json())
       .then((data) => {
-        const arrayDataFiltered = category
+        const filteredData = category
           ? data.filter((item) => item[category] === value)
           : data;
 
-        localStorage.setItem("goods", JSON.stringify(arrayDataFiltered));
+        localStorage.setItem("goods", JSON.stringify(filteredData));
 
         if (window.location.pathname !== "/goods.html") {
           window.location.href = "/goods.html";
         } else {
-          renderGoods(arrayDataFiltered);
+          renderGoods(filteredData);
         }
       });
   };
@@ -37,6 +61,13 @@ const getGoods = () => {
     window.location.pathname === "/goods.html"
   ) {
     renderGoods(JSON.parse(localStorage.getItem("goods")));
+  }
+
+  if (more) {
+    more.addEventListener("click", (e) => {
+      e.preventDefault();
+      getData();
+    });
   }
 };
 
